31508161af4e7f96fec05ea154abd6b610fd78ee
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-05 21:55:40 +0200

    comaparision file

---
+++ comparision.js
@@ -0,0 +1,41 @@
+// // // console.log(5<6);
+// // console.log(10<6);
+// // console.log(10>6);
+// // console.log(6 == 6);
+// // console.log(5 ==6);
+// // console.log(10<14);
+// // console.log(5 != 5);
+// // console.log( 7 != 6);
+// // less than or equal
+// // console.log( 5<=6);
+// // console.log(6<=6);
+// // console.log(10<=6);
+// // // 10 obossy 6 er take boro
+
+// // console.log(10 == 10);
+// // console.log(29 >= 30);
+// // console.log(29 >= 25);
+// // console.log(29 >= 25);
+// // console.log(29 >= 25);
+// // console.log(29 >= 25);
+// // console.log(29 >= 25);
+
+// // greater than or equal
+// console.log(5 >= 6)
+// console.log(5 >= 2)
+// console.log(5 >= 5)
+// console.log(5 >= 7)
+
+var num1 = 40;
+var num2 = 80;
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 == num2)
+console.log(num1 !=num2)
+console.log(num1 <= num2)
+console.log(num1 >= num2)
+
+// multiple conditions
+// money1 > money2 && result1 > result2 && height1 > height2
+
+money1 > money2 || result1 > result2 || height1 > height2
