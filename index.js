0639d60618d332c8591a102eed86492eca7bca80
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-05 20:44:33 +0200

    index file

---
+++ index.js
@@ -0,0 +1,19 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// 1. get element by index
+var element = numbers[5];
+console.log(element);
+
+// 2.set element value by index
+numbers[3] = 39;
+numbers[5] = 10;
+console.log(numbers);
+// 3. find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
+
+var newAddress = numbers.indexOf(45);
+console.log(newAddress);
+
+var arrayElements = [ 10, 20, 30, 40, 50, 60, 70, 80, 90,100];
+console.log(arrayElements.length);
