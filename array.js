834d1c2e3ef7f47fc00cf43b3d78e6cf30875602
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-05 20:16:39 +0200

    first array

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFee = [5000, 2000, 400];
+var nayiKas = ['mahi', 'opu', 'sabana' ];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+// console.log(vowels.length);
+console.log(picnicFee);
+console.log(friendsAge.length);
---
+++ variable.js
@@ -0,0 +1,15 @@
+
+// Number type variable
+// var sunglassPrice = 120;
+
+// // string type variable
+// var specialOneName = "Ariana Grande";
+
+
+// // boolean type variable
+// var isSingle = true;
+
+// // two types of number: Integer: 120;
+// // Float,decimal: 3.65;
+
+// variable naming convention: rules , best practie
