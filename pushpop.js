9b54c605c5f0c13cc8960623885120d52cf5506c
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-05 21:16:50 +0200

    pushpop

---
+++ pushpop.js
@@ -0,0 +1,17 @@
+var numbers = [78, 45, 98, 45];
+numbers.push(49);
+// console.log(numbers);
+
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('shamim');
+friends.push('kinlam');
+console.log(friends);
+numbers.pop();
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+numbers.log();
+console.log(numbers);
+
+// use push to add element to an array as the last element array 
+// use pop to get last element
