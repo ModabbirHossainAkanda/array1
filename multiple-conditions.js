191db2db9cd67b2bc771729e1bdcaa797aa18f52
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-07 19:52:31 +0200

    multiple-conditions

---
+++ multiple-conditions.js
@@ -0,0 +1,27 @@
+var isGraduated = false;
+salary = 7000;
+car = 1;
+
+// condition 1 done 
+// // if(isGraduated == true){
+// //     console.log('Eso biye kore feli');
+// // }
+// // else{
+// //     console.log('tor kopale biye nai');
+// // }
+
+// // if(isGraduated == true && salary >50000 && car >= 1){
+// //     console.log('eso biye kore feli');
+// // }
+// // else{
+// //     console.log('tor kopale biye nai');
+// // }
+
+
+// Condition 2
+if(isGraduated === true || salary > 50000 || car > 1){
+    console.log('Eso biye kore feli');
+}
+else{
+    console.log('Tomar friend er number dao');
+}
