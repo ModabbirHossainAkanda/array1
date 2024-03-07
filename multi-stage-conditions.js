bec062d16b22a3202f04efb6f60677aaccb2ea11
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-07 20:14:11 +0200

    multi-stage-conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 30;
+var danishPrice = 45;
+var butterBread =35;
+var toastBiscuit = 20;
+if(danishPrice < money){
+    console.log('Danish khamu')
+}
+else if(butterBread < money){
+    console.log('Butter bon khamu');
+}
+else if(toastBiscuit < money){
+    console.log('biscuit khamu');
+}
+else {
+    console.log('khali chai sastheir jnno valo');
+}
