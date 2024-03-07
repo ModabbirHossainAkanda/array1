4170dcc49ecfabd0ae3be2d4be6c40fa5a150993
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-07 19:12:55 +0200

    conditional

---
+++ conditional.js
@@ -0,0 +1,24 @@
+var iphonePrice = 80000;
+var myBudget = 90500;
+// if iphone price is less than my budget. I will buy the iphone 
+// If iphone price is less than my budget I will buy the iphone 
+// If(condition) {
+//     will excute if the condition is true }
+  if(iphonePrice < myBudget) {
+    console.log( 'i phone kine futani marbo');
+}
+var chickenPrice = 500;
+var myMoney = 200;
+if(chickenPrice >= myMoney){
+    console.log('Murgir raan khamu and haddi chibamu!!');
+}
+if(chickenPrice > myMoney){
+    console.log('Smashed potato and lentils soup');
+}
+
+if(chickenPrice > myMoney) {
+    console.log('I will eat chicken');
+}
+else{
+    console.log('I will eat potato');
+}
