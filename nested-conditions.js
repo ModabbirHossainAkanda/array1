825ba66141e98efd31151efee7bbae20282ee3fe
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-07 20:22:12 +0200

    nested-conditions

---
+++ nested-conditions.js
@@ -0,0 +1,15 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if(math == true){
+    if(geometry == true){
+        
+    }
+    else{
+        console.log('pithagorous hoite parba na')
+    }
+}
+else{
+
+}
