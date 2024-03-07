435c6098d517ff2c75e70437b1388b44b0ab5d31
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-07 21:47:47 +0200

    practice

---
+++ practice.js
@@ -0,0 +1,38 @@
+// problem 01
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// console.log(fruits.length);
+// var index = fruits[1];
+// console.log(index);
+// fruits.push('lichu');
+// console.log(fruits);
+// fruits.push('mango');
+// console.log(fruits);
+// fruits[2] = 'watermelon';
+// console.log(fruits)
+
+// problem 02
+
+// var myScore = 10;
+// var totalScore = 66;
+// var janeScore = 95;
+// var peterScore = 56;
+// var johnScore = 40;
+// if(totalScore < myScore){
+//     console.log('gpa 5');
+// }
+// else if(janeScore < myScore){
+//     console.log('fail the subject');
+// }
+// else if(peterScore < myScore){
+//     console.log('two subject fail');
+// }
+// else if(johnScore < myScore){
+//     console.log('helmet kinum');
+// }
+// else
+// {
+//     console.log('amra kichui kinbo na');
+// }
+
+// problem 03
+
